9a5b5c453e48bc9c6d958b77ed96a66e463cee0b
Leonardo Souto <leo.soutomaior@hotmail.com>
2024-02-06 10:50:57 -0300

    2° commit-

--- livro/views.py
+++ livro/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from usuarios.models import Usuario
 
 
-def cadastrar(request):
-    return HttpResponse('Jjj')
+def home(request):
+    if request.session.get('usuario'):
+        usuario = Usuario.objects.get(id = request.session['usuario']).nome
+        return HttpResponse(f'olá {usuario}')
+    else:
+        return redirect('/auth/login/?status=2')
