6d610bc4a685f053d2a0e64d50c80f93e5a54d12
Leonardo Souto <leo.soutomaior@hotmail.com>
2024-02-07 11:01:52 -0300

    3° commit-

--- livro/views.py
+++ livro/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from usuarios.models import Usuario
+from .models import Livros
 
 
 def home(request):
     if request.session.get('usuario'):
-        usuario = Usuario.objects.get(id = request.session['usuario']).nome
-        return HttpResponse(f'olá {usuario}')
+        usuario = Usuario.objects.get(id = request.session['usuario'])
+        livros = Livros.objects.filter(usuario = usuario)
+        return render(request, 'home.html', {'livros': livros})
     else:
         return redirect('/auth/login/?status=2')
+    
+
+def ver_livros(request, id):
+    livros = Livros.objects.get(id = id)
+    return render(request, 'ver_livro.html', {'livro': livros})
