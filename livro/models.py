99795608fe6367faf243607555032ff7a4260c00
Leonardo Souto <leo.soutomaior@hotmail.com>
2024-02-08 15:03:50 -0300

    4° commit

--- livro/models.py
+++ livro/models.py
@@ -5,9 +5,12 @@ from usuarios.models import Usuario
 class Categoria(models.Model):
     nome = models.CharField(max_length=30)
     descricao = models.TextField()
+    usuario = models.ForeignKey(Usuario, on_delete=models.DO_NOTHING)
     
     def __str__(self) -> str:
         return self.nome
+    
+
 
 class Livros(models.Model):
     nome = models.CharField(max_length = 100)
@@ -15,10 +18,6 @@ class Livros(models.Model):
     co_autor = models.CharField(max_length = 30 , blank = True)
     data_cadastro = models.DateField(default = date.today)
     emprestado = models.BooleanField(default = False)
-    nome_emprestado = models.CharField(max_length = 30, blank = True, null = True)
-    data_emprestimo = models.DateTimeField(blank = True, null = True)
-    data_devolucao = models.DateTimeField(blank = True, null = True)
-    tempo_duracao = models.DateField(blank = True, null = True)
     categoria = models.ForeignKey(Categoria, on_delete = models.DO_NOTHING)
     usuario = models.ForeignKey(Usuario, on_delete = models.DO_NOTHING)
     
@@ -27,3 +26,15 @@ class Livros(models.Model):
         
     def __str__(self):
         return self.nome
+    
+
+
+class Emprestimos(models.Model):
+    nome_emprestado = models.ForeignKey(Usuario, on_delete=models.DO_NOTHING, blank = True, null = True)
+    nome_emprestado_anonimo = models.CharField(max_length = 30, blank = True, null = True)
+    data_emprestimo = models.DateField(blank = True, null = True)
+    data_devolucao = models.DateField(blank = True, null = True)
+    livro = models.ForeignKey(Livros, on_delete=models.DO_NOTHING)
+    
+    def __str__(self) -> str:
+        return f"{self.nome_emprestado} | {self.livro}"
